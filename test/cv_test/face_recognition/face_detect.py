3c4bc5050f1bb3a183cd8e43bb5f603496635b44
OcsphyDesktop <ocsphy@qq.com>
2020-02-13 17:00:38 +0800

    check every 5 frames

--- test/cv_test/face_recognition/face_detect.py
+++ test/cv_test/face_recognition/face_detect.py
@@ -36,14 +36,14 @@ known_face_encodings, known_face_names = load_sample(sample_path)
 face_locations = []
 face_encodings = []
 face_names = []
-process_this_frame = True
+process_this_frame = 1
 
 resol = (320, 240)
 camera = picamera.PiCamera()
 camera.resolution = resol
 camera.framerate = 20
 raw_capture = PiRGBArray(camera, size= resol)
-
+skrink = 0.5
 
 for frame in camera.capture_continuous(
         raw_capture, format="bgr", use_video_port=True):
@@ -52,13 +52,13 @@ for frame in camera.capture_continuous(
     frame_image = frame.array
 
     # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame_image, (0, 0), fx=0.25, fy=0.25)
+    small_frame = cv2.resize(frame_image, (0, 0), fx=skrink, fy=skrink)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
 
     # Only process every other frame of video to save time
-    if process_this_frame:
+    if process_this_frame == 1:
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
@@ -82,15 +82,18 @@ for frame in camera.capture_continuous(
 
             face_names.append(name)
 
-    process_this_frame = not process_this_frame
+    # check every 5 frames
+    process_this_frame += 1
+    if process_this_frame > 5:
+        process_this_frame = 1
 
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+        top *= 1 / skrink
+        right *= 1 / skrink
+        bottom *= 1 / skrink
+        left *= 1 / skrink
 
         # Draw a box around the face
         cv2.rectangle(frame_image, (left, top), (right, bottom), (0, 0, 255), 2)
